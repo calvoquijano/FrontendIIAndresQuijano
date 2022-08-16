1ef641816482450e85e7ad556f19d008ba6b194b
calvoquijano <familiaqc@MacBook-Air-de-Andrea.local>
2022-08-16 13:56:43 -0500

    Subí el repaso de la clase 01

---
+++ clase01Repaso/funciones.js
@@ -0,0 +1,19 @@
+//console log, operaciones y alertas
+console.log("hola,como vas?");
+console.error("Esto es un error");
+console.warn("Esto es una advertencia");
+let objeto = {
+    mensaje: "Hola soy un tipo de tabla",
+    mensaje2: "Tabla de objetos"
+};
+let arreglo = ["Hola soy un tipo de tabla", "Tabla de arreglos", "Otro mensaje"]
+console.table(objeto);
+console.table(arreglo);
+
+let n1 = 5;
+let n2 = 4;
+let resultado = 5+4;
+
+alert("hola,como vas? Esto es una alerta, y " + n1 +" + "+ n2 + " es igual a " + resultado);
+
+
